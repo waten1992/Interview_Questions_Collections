be4198215442bd1df030a4d8aded00bcd404aac6
waten <waten1992@gmail.com>
2015-02-07 22:16:31 +0800

    删除多余的测试信息

--- 剔除A串中B串的字符.cpp
+++ 剔除A串中B串的字符.cpp
@@ -24,7 +24,6 @@ void Print_Binary_Num(int Num) //输出二进制 ， 按照小端序列输出
             cout<<'\t';
     }
 }
-
 char * Handler(char *s1 , char *s2)
 {
     int  i = 0 , Tmp = 0 , check = 0 , index = 0;
@@ -32,41 +31,27 @@ char * Handler(char *s1 , char *s2)
     while (s1[i]) //HASH --- A 相应的位置1
     {
         Tmp = (s1[i] - 'a') % 32 ; // HASH
-
         if (Tmp >= 0 && Tmp <=25) //排出其他字符
             check = (1<<Tmp)|check;
-#if 0
-        cout<<"-----"<<check<<endl;
-        Print_Binary_Num(check);
-#endif
         i++;
     }
     i = 0; //再次初始化
     while (s2[i]) //HASH ---B ----把A 和 B中都存在的字符 都置0
     {
         Tmp = (s2[i] - 'a') % 32 ;
-
         if (Tmp >= 0 && Tmp <=25) //排出其他字符
             check = (~(1<<Tmp))&check;//---1 两种方式都可以，都是把相应的为清零 ； 推荐用第一种
-          //check = (1<<Tmp)^check;  //----2 但是第二种有副作用，假若A不存在,而B存在；
-                                    // 则check的相应位被置1，不过在本例中，没有影响，后面是相与
-#if 0
-        cout<<"-----"<<check<<endl;
-        Print_Binary_Num(check);
-#endif
+        //check = (1<<Tmp)^check;  //----2 但是第二种有副作用，假若A不存在,而B存在；
+        // 则check的相应位被置1，不过在本例中，没有影响，后面是相与
         i++;
     }
     i = 0; //第3次初始化
     while(s1[i]) //再次 扫描A 和check 位相与 。赋值给临时数组
     {
         Tmp = (s1[i] - 'a') % 32 ;
-
         if ((1<<Tmp)&check)
         {
             Tmp_String[index] = s1[i];
-#if 0
-            cout<<"This is --->"<<Tmp_String[index]<<endl;
-#endif
             index++;
         }
         i++;
